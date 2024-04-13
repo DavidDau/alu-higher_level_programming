8ecb597f4834127d1f3d593de5190051d21d7843
root <d.cyubahiro1@alustudent.com>
2024-04-13 08:58:51 -0700

    Now

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 """ORM"""
-import sys
-from model_state import Base, State
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import Column, Integer, String, create_engine
+"""SQLalchemy"""
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """Class AKA table"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+# Define engine to connect to MySQL server
+engine = create_engine('mysql://username:password@localhost:3306/database')
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Create all tables in the database
     Base.metadata.create_all(engine)
